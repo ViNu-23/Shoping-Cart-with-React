19f16704960f438ba7b6857e4b7a5c728fe50e17
ViNu-23 <vijayvinu46@gmail.com>
2023-07-31 17:01:29 +0530

    fix: added icons

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,10 @@
-import { BsCart4,BsFillHouseHeartFill } from "react-icons/bs";
+import { BsCart4,BsFillHouseHeartFill,BsGlobeAmericas } from "react-icons/bs";
 import "./Navbar.css";
 import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="nav">
+            <NavLink to="#">< BsGlobeAmericas /> The REAL Store</NavLink>
       <NavLink to="/"> <BsFillHouseHeartFill />   HOME</NavLink>
       <NavLink to="/cart"> <BsCart4 />  CART</NavLink>
     </div>
